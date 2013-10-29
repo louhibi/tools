d104bc76e52ffe530f9761d87c60b7cd51642eac
Mohammed Rida Louhibi <mrlouttibi@Desktop41.(none)>
2013-10-29 14:33:53 -0400

    first commit

---
+++ tools.py
@@ -0,0 +1,109 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- 
+
+
+import yaml
+import os
+import traceback
+
+class Error(Exception):
+    def __init__(self, value, trace, **args):
+        self.value = value
+        self.trace = trace
+        self.initHook(**args)
+
+    def initHook(self):
+        pass
+
+    def __str__(self):
+        return repr(self.value)
+
+
+class Error_File_Yaml(Error):
+    def initHook(self):
+        self.value = "YAML Error: Fail to open the Yaml file %s" % self.value
+
+class Error_import_app(Error):
+    def initHook(self):
+        self.value = "Module Loader Error: Fails to load %s" % self.value
+
+def LoadYaml(pathFile):
+    """
+    Loads the Yaml file and returns it as Yaml object
+    """
+    # checking if the file exists else raise error file does not exists
+    if os.path.exists(pathFile):
+        with open(pathFile) as f:
+            dataToReturn = yaml.safe_load(f)
+        return Yaml_object(**dataToReturn)
+
+    else:
+        raise Error_File_Yaml(pathFile, traceback.format_exc())
+
+def ipv4_check(ip):
+    """
+    @returns True if valid ip v4 else return False
+    """
+    ipParts = ip.split(".")
+    if len(ipParts) != 4:
+        return False
+    for part in ipParts:
+        if not 0 <= int(part) <= 255:
+            return False
+    return True
+
+def cleanFormatNumber(number):
+    """
+    Cleans Format a Number
+    cleanFormatNumber(514-546-1234) returns 5145461234
+    or cleanFormatNumber(514 546 1234) returns 5145461234
+    """
+    return "".join(number.replace(" ", "").split('-'))
+
+def cleanFormatListNumber(Listnumber):
+    """
+    Cleans Format a list of Numbers
+    cleanFormatNumber(514-546-1234) returns 5145461234
+    or cleanFormatNumber(514 546 1234) returns 5145461234
+    """
+    # print [i for i in Listnumber]
+    return [cleanFormatNumber(i) for i in Listnumber]
+
+def import_module(self, app):
+    """
+    @returns the the imported module or it raises an error
+    """
+    try:
+        return __import__('apps.%s' % app, globals(), locals(), [app], -1)
+    except ImportError:
+        raise Error_import_app(app, traceback.format_exc())
+
+
+class Yaml_object:
+    """
+    object Yaml file.
+    y = YamlFile
+    print y.property
+    print y.property.property1
+    """
+    def __init__(self, **entries):
+        self.dictionary = entries
+        for k, v in entries.items():
+            if not isinstance(v, dict):
+                setattr(self, k, v)
+            else:
+                setattr(self, k, Yaml_object(**v))
+    def __str__(self):
+        return "<Yaml_object: %s>" % repr(self.dictionary)
+
+
+if __name__ == '__main__':
+    assert ipv4_check("12.14.213.13") == True
+    assert ipv4_check("12.14.213.256") == False
+    assert ipv4_check("12.14.213.5.13") == False
+    assert ipv4_check("12.14.213") == False
+    assert cleanFormatNumber("514-546-1234") == "5145461234"
+    assert cleanFormatNumber("514 546 1234") == "5145461234"
+    assert cleanFormatNumber("514 546-1234") == "5145461234"
+    assert cleanFormatNumber("5145461234") == "5145461234"
+    assert cleanFormatListNumber(["5145461234", "514-546-1234", "514 546-1234"]) == ["5145461234", "5145461234", "5145461234"]
